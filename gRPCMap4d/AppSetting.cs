2b8a11492eb967b3b546fcba3cd6fe44ed7ca3d9
trungvu0128 <trungvu0128@gmail.com>
2023-02-10 15:18:21 +0700

    implement search account

---
+++ gRPCMap4d/AppSetting.cs
@@ -0,0 +1,71 @@
+﻿namespace gRPCMap4d
+{
+    /// <summary>
+    /// Appsettings
+    /// </summary>
+    public class AppSetting
+    {
+        public KeyCloak KeyCloak { get; set; } = default!;
+    }
+
+    /// <summary>
+    /// Key Cloak
+    /// </summary>
+    public class KeyCloak
+    {
+        /// <summary>
+        /// Public Key
+        /// </summary>
+        public string PublicKey { get; set; } = default!;
+
+        /// <summary>
+        /// Key Cloak Endpoint
+        /// </summary>
+        public string KeyCloakEndpoint { get; set; } = default!;
+
+        /// <summary>
+        /// Master Endpoint
+        /// For authen to get token
+        /// </summary>
+        public string MasterEndpoint { get; set; } = default!;
+
+        /// <summary>
+        /// Max result when request to keyCloak
+        /// </summary>
+        public byte MaxResultRequest { get; set; }
+
+        /// <summary>
+        /// ClientIdMaster
+        /// </summary>
+        public string ClientIdMaster { get; set; } = default!;
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string UserName { get; set; } = default!;
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        public string PassWord { get; set; } = default!;
+
+        /// <summary>
+        /// Realm
+        /// </summary>
+        public string Realm { get; set; } = default!;
+
+        /// <summary>
+        /// Client Endpoint
+        /// </summary>
+        public string ClientEndpoint { get; set; } = default!;
+
+        /// <summary>
+        /// Client Id
+        /// </summary>
+        public string ClientId { get; set; } = default!;
+
+        /// <summary>
+        /// Client Secret
+        /// </summary>
+        public string ClientSecret { get; set; } = default!;
+    }
+}
---
+++ gRPCMap4d/Models/KeyCloak/TokenResponse.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json.Serialization;
+
+namespace gRPCMap4d.Models.KeyCloak;
+/// <summary>
+/// Token response model
+/// </summary>
+public class TokenResponse
+{
+    /// <summary>
+    /// Token response
+    /// </summary>
+    public TokenResponse()
+    {
+        ModifyTime = DateTime.Now;
+    }
+
+    /// <summary>
+    /// Access token
+    /// </summary>
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; } = default!;
+
+    /// <summary>
+    /// Refresh token
+    /// </summary>
+    [JsonPropertyName("refresh_token")]
+    public string RefreshToken { get; set; } = default!;
+
+    /// <summary>
+    /// Api domain
+    /// </summary>
+    [JsonPropertyName("api_domain")]
+    public string ApiDomain { get; set; } = default!;
+
+    /// <summary>
+    /// Token type
+    /// </summary>
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; } = default!;
+
+    /// <summary>
+    /// Expires in
+    /// </summary>
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+
+    /// <summary>
+    /// Expires in
+    /// </summary>
+    [JsonPropertyName("refresh_expires_in")]
+    public int RefreshExpiresIn { get; set; }
+
+    /// <summary>
+    /// Expires in
+    /// </summary>
+    [JsonPropertyName("expiresInDateTime")]
+    public DateTime ExpiresInDateTime
+    {
+        get
+        {
+            var time = ModifyTime.AddSeconds(ExpiresIn);
+            return time;
+        }
+    }
+
+    /// <summary>
+    /// Set date create object
+    /// </summary>
+    private DateTime ModifyTime { get; set; }
+}
+
