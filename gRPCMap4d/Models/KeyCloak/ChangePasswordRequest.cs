7772ab166fb0c717a067d04d2cfb834949111e6f
trungvu0128 <trungvu0128@gmail.com>
2023-02-10 15:18:21 +0700

    implement search account

---
+++ gRPCMap4d/Models/KeyCloak/ChangePasswordRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace gRPCMap4d.Models.KeyCloak
+{
+    public class ChangePasswordRequest
+    {
+        /// <summary>
+        /// Type
+        /// </summary>
+        [JsonPropertyName("type")]
+        public string? Type { get; set; }
+
+        /// <summary>
+        /// Value new password
+        /// </summary>
+        [JsonPropertyName("value")]
+        public string? Value { get; set; }
+
+        /// <summary>
+        /// Temporary
+        /// </summary>
+        [JsonPropertyName("temporary")]
+        public bool Temporary { get; set; }
+    }
+}
---
+++ gRPCMap4d/Utils/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace gRPCMap4d.Utils
+{
+    public class Constants
+    {
+        public const string KeyCloakHttpClient = "KeyCloakHttpClient";
+
+        public const string Exception = "Exception";
+
+        public const string ApplicationJsonAccept = "application/json";
+
+        public const string Password = "Password";
+    }
+}
